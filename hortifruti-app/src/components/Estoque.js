c3433003b09db29fbe70c21df143bdbb8b87e6bb
asaph-t-Souza <asaph.net@gmail.com>
2023-06-01 19:46:34 -0300

    a

--- hortifruti-app/src/components/Estoque.js
+++ hortifruti-app/src/components/Estoque.js
@@ -1,24 +1,36 @@
 import TabelaProdutos from "./TabelaProdutos";
+import {useState} from 'react';
 
 export default function Estoque({produtos}) {
+
+    const [filtro, setFiltro] = useState("");
+    const [somenteEstoque, setSomenteEstoque] = useState(false);
+
     return(
         <>
-            <BarraPesquisa/>
+            <BarraPesquisa 
+                filtro={filtro} 
+                aoModificarFiltro={setFiltro}
+                somenteEstoque={somenteEstoque}
+                aoModificarEstoque={setSomenteEstoque}
+            />
             <TabelaProdutos produtos={produtos} />
         </>
     );
 }
 
-function BarraPesquisa(){
+function BarraPesquisa({filtro, somenteEstoque}){
     return(
         <form>
         <input
             type="text" 
-            placeholder="Pesquisar..."            
+            placeholder="Pesquisar..."
+            value={filtro}         
         />
         <label>
             <input
                 type="checkbox"
+                checked={somenteEstoque}
             />
             Somente mostrar produtos em estoque
         </label>
